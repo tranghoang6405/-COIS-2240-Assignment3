4c6f5afcb923adf3df24455dcde4bf70bac394ab
tranghoang6405 <tranghoangthu.05@gmail.com>
2024-11-28 17:33:07 -0500

    Refractor the Transaction class to follow Singleton.

--- Library/src/Transaction.java
+++ Library/src/Transaction.java
@@ -2,9 +2,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
+	// instance of the class
+	private static Transaction instance;
+	private Transaction() {}
+	
+	// Get the single instance of the class
+	public static Transaction getTransaction() {
+		if (instance == null) {
+			synchronized (Transaction.class) {
+				if (instance == null) {
+					instance = new Transaction();
+				}
+			}
+		}
+		return instance;
+	}
 
     // Perform the borrowing of a book
-    public static boolean borrowBook(Book book, Member member) {
+    public boolean borrowBook(Book book, Member member) {
         if (book.isAvailable()) {
             book.borrowBook();
             member.borrowBook(book); 
@@ -18,7 +33,7 @@ public class Transaction {
     }
 
     // Perform the returning of a book
-    public static void returnBook(Book book, Member member) {
+    public void returnBook(Book book, Member member) {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
@@ -30,7 +45,7 @@ public class Transaction {
     }
 
     // Get the current date and time in a readable format
-    private static String getCurrentDateTime() {
+    private String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
     }
